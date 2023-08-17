17c7bee0276c1284a3a25aa29f6d96f3dd023e40
HuyDV <huycntt03@gmail.com>
2023-08-17 20:53:23 +0700

    ✨ add handle init data room and service

---
+++ src/test/java/com/bookinghotel/BookingHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookinghotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHotelApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
