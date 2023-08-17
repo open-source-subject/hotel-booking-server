1ef6bcdcc323dd04fbc8921d6a4c4b91a5abd18d
HuyDV <huycntt03@gmail.com>
2023-08-17 21:12:00 +0700

    🔒 update authentication entry point

--- src/main/java/com/bookinghotel/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/bookinghotel/security/JwtAuthenticationEntryPoint.java
@@ -1,8 +1,9 @@
 package com.bookinghotel.security;
 
-import com.bookinghotel.base.VsResponseUtil;
+import com.bookinghotel.base.RestData;
 import com.bookinghotel.constant.ErrorMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,20 +13,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @RestControllerAdvice
 public class JwtAuthenticationEntryPoint implements org.springframework.security.web.AuthenticationEntryPoint {
 
+    @SneakyThrows
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        ServletServerHttpResponse res = new ServletServerHttpResponse(response);
-        res.setStatusCode(HttpStatus.UNAUTHORIZED);
-        res.getServletResponse().setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        res.getBody().write(mapper.writeValueAsString(VsResponseUtil.error(HttpStatus.UNAUTHORIZED,
-                ErrorMessage.UNAUTHORIZED)).getBytes());
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
+        try (ServletServerHttpResponse res = new ServletServerHttpResponse(response)) {
+            res.setStatusCode(HttpStatus.UNAUTHORIZED);
+            res.getServletResponse().setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+            RestData<?> body = RestData.error(ErrorMessage.UNAUTHORIZED);
+            res.getBody().write(new ObjectMapper().writeValueAsBytes(body));
+        }
     }
-
 }
